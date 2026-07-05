85dd422ee3f77feb0673b690a933771dc9a0bf5c
jbkasper5 <jbkasper03@gmail.com>
2026-07-05 11:50:55 -0500

    exec-ing binaries from disk seems to work, but leaves A LOT of un-cleanable garbage in RAM

--- src/kernel/src/filesystem/elf.c
+++ src/kernel/src/filesystem/elf.c
@@ -1,4 +1,5 @@
 #include "filesystem/elf.h"
+#include "system/entry.h"
 
 extern void do_user_things();
 
@@ -36,10 +37,9 @@ void readelf(file_t* file){
 
     // allocate process metadata
     // should now have a valid L0 page table and stack
-    pcb_t* process = procalloc((u64) header->e_entry);
+    // pcb_t* process = procalloc((u64) header->e_entry);
 
-    // set process's entrypoint
-    process->registers.pc = header->e_entry;
+    u64* new_proc_l0 = buddy_alloc_pt();
 
     elf64_program_header* program_header = UNSCALED_POINTER_ADD(header, header->e_phoff);
 
@@ -58,7 +58,7 @@ void readelf(file_t* file){
 
         u16 order = log2_pow2(program_header->p_memsz / 4096);
 
-        u64 phys_block = buddy_alloc(program_header->p_memsz); // 3ffd7000
+        u64 phys_block = buddy_alloc(program_header->p_memsz);
 
         // map physical block into kernel memory so we can set up the process
         map(phys_block, va_to_pa(phys_block), order, MAP_KERNEL | MAP_WRITE, L0_TABLE);
@@ -70,37 +70,42 @@ void readelf(file_t* file){
 
         memcpy(UNSCALED_POINTER_ADD(phys_block, in_page_offset), rootfs.block_buf, program_header->p_filesz);
 
-        map(program_header->p_vaddr, va_to_pa(phys_block), order, flags, process->registers.ttbr); // 0x400000 -> 3ffd7000
+        // map the program sections into the new L0 table for the current process
+        map(program_header->p_vaddr, va_to_pa(phys_block), order, flags, new_proc_l0); // 0x400000 -> 3ffd7000
 
         program_header++;
     }
 
-    add_to_schedule(process);
-    
+    pcb_t* current = get_current();
 
-    seek(file, header->e_shoff, SEEK_SET);
-    read(file, block, sizeof(ext4_block));
+    // needs updating to the location of the ELF maybe
+    // current->cwd
 
-    elf64_section_header* section_header = (elf64_section_header*) block;
+    // switch over ttbr
+    u64* old_ttbr = current->registers.ttbr;
 
-    // read the string table into memory
-    char* string_table = (char*) kmalloc(sizeof(ext4_block));
-    seek(file, (section_header + string_table_section_header)->sh_offset, SEEK_SET);
-    read(file, string_table, (section_header + string_table_section_header)->sh_size);
+    current->registers.ttbr = va_to_pa(new_proc_l0);
 
-    section_header = (elf64_section_header*) block;
-    u8 requires_allocation;
-    for(int i = 0; i < section_header_count; i++){
-        requires_allocation = section_header->sh_flags & SHF_ALLOC ? TRUE : FALSE;
-        INFO("Section header %d: \n", i + 1);
-        INFO("  Name: %s\n", string_table + section_header->sh_name);
-        INFO("  Type: %d\n", section_header->sh_type);
-        INFO("  Flags: 0x%x\n", section_header->sh_flags);
-        INFO("  Address: 0x%x\n", section_header->sh_addr);
-        INFO("  Offset: %d\n", section_header->sh_offset);
-        INFO("  Size: %d\n", section_header->sh_size);
-        INFO("  \e[31mREQUIRES MAPPING:\e[0m %d\n", requires_allocation);
+    // tear down old_ttbr
+    // teardown_vm(old_ttbr);
 
-        section_header++;
-    }
+    // read trapframe to set the new process proper PC
+    u64* trapframe = (u64*)(ALIGN_UP(current->kernel_stack, PAGE_SIZE) - 0x10 - S_FRAME_SIZE);
+    trapframe[31] = 0x0000800000000ULL - 16;           // SP_EL0
+    trapframe[32] = header->e_entry;                   // ELR_EL1
+    trapframe[33] = 0x0;                               // SPSR
+
+
+    u32 stack_size = PAGE_SIZE * 2;
+    u64 stack_base = buddy_alloc(stack_size);
+
+    // map new user stack
+    map(0x0000800000000ULL - stack_size, va_to_pa(stack_base), 1, MAP_USER | MAP_READ | MAP_WRITE, new_proc_l0);
+    // 0x800000000
+    // 0x7fffffff0
+
+    // swap the base table for the process
+    switch_user_tlb(current->registers.ttbr);
+
+    // update user sp, pc, and zero out other registers
 }
